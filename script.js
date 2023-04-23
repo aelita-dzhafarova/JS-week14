3ff5b17bd1ffd2d13c0f7343c746b4f7d49550c0
Aelita <119422803+aelita-dzhafarova@users.noreply.github.com>
2023-04-24 00:03:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Intersection function for 2 arrays
 function getIntersection2(arr1, arr2) {
-    return [arr1, arr2]?.reduce((acc, currentValue) => {
-      return acc.filter(res => currentValue.includes(res));
-    })
-  }
-  
-  // Intersection function for 3 arrays
-  function getIntersection3(arr1, arr2, arr3) {
-    return [arr1, arr2, arr3]?.reduce((acc, currentValue) => {
-      return acc.filter(res => currentValue.includes(res));
-    })
-  }
+  return [arr1, arr2].reduce((acc, currentValue) => {
+    return acc.filter(res => currentValue.includes(res));
+  })
+}
+
+// Intersection function for 3 arrays
+function getIntersection3(arr1, arr2, arr3) {
+  return [arr1, arr2, arr3].reduce((acc, currentValue) => {
+    return acc.filter(res => currentValue.includes(res));
+  })
+}
